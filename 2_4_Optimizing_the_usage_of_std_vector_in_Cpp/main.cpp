65c3b280edf5ee0d3f5babaa7c1df2b53ebe6130
kavinda-ravishan <kvnravishan@outlook.com>
2021-12-13 16:49:25 +0530

    Optimizing_the_usage_of_std_vector_in_Cpp

---
+++ 2_4_Optimizing_the_usage_of_std_vector_in_Cpp/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+struct Vertex
+{
+    float x, y, z;
+
+    Vertex(float x, float y, float z) : x(x), y(y), z(z) {}
+
+    Vertex(const Vertex &vertex) : x(vertex.x), y(vertex.y), z(vertex.z)
+    {
+        std::cout << "Copied!\n";
+    }
+};
+
+int main()
+{
+    std::cout << "Not potimized\n";
+    std::vector<Vertex> vec1;
+
+    vec1.push_back(Vertex(1, 2, 3));
+    vec1.push_back(Vertex(4, 5, 6));
+    vec1.push_back(Vertex(7, 8, 9));
+    std::cout << "\n";
+
+    std::cout << "Potimized\n";
+    std::vector<Vertex> vec2;
+    vec2.reserve(3);
+
+    vec2.emplace_back(1, 2, 3);
+    vec2.emplace_back(4, 5, 6);
+    vec2.emplace_back(7, 8, 9);
+
+    return 0;
+}
